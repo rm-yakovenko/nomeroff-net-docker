fa7621b11fe532174f14f4599f3610a9a1e6d0b7
Rm Yakovenko <rm.yakovenko@gmail.com>
2020-09-11 15:47:00 +0300

    15 Migrate to nomeroff v1.0

--- app.py
+++ app.py
@@ -5,49 +5,36 @@ from urllib.request import urlopen
 import matplotlib.image as mpimg
 import tensorflow as tf
 
-# https://github.com/tensorflow/tensorflow/issues/28287#issuecomment-495005162
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-session = tf.Session(config=config)
-sess = tf.Session()
-graph = tf.get_default_graph()
-
-from tensorflow.python.keras.backend import set_session
-set_session(sess)
-
 warnings.filterwarnings('ignore')
 
 # change this property
-NOMEROFF_NET_DIR = os.path.abspath('./nomeroff-net')
-
-# specify the path to Mask_RCNN if you placed it outside Nomeroff-net project
-MASK_RCNN_DIR = os.path.join(NOMEROFF_NET_DIR, 'Mask_RCNN')
-MASK_RCNN_LOG_DIR = os.path.join(NOMEROFF_NET_DIR, 'logs')
-
+NOMEROFF_NET_DIR = os.path.abspath('../nomeroff-net')
 sys.path.append(NOMEROFF_NET_DIR)
 
 # Import license plate recognition tools.
-from NomeroffNet import filters, RectDetector, TextDetector, OptionsDetector, Detector, textPostprocessing
+from NomeroffNet import  Detector
+from NomeroffNet import  filters
+from NomeroffNet import  RectDetector
+from NomeroffNet import  OptionsDetector
+from NomeroffNet import  TextDetector
+from NomeroffNet import  textPostprocessing
 
-nnet = Detector(MASK_RCNN_DIR, MASK_RCNN_LOG_DIR)
-nnet.loadModel('latest')
-
+# load models
 rectDetector = RectDetector()
 
 optionsDetector = OptionsDetector()
-optionsDetector.load('latest')
+optionsDetector.load("latest")
 
 textDetector = TextDetector.get_static_module("eu")()
 textDetector.load("latest")
 
+nnet = Detector()
+nnet.loadModel(NOMEROFF_NET_DIR)
 
 def read_number_plates(url):
-    global graph, sess
     with urlopen(url) as file:
         img = mpimg.imread(file, 0)
 
-    with graph.as_default():
-        set_session(sess)
         NP = nnet.detect([img])
 
         # Generate image mask.
